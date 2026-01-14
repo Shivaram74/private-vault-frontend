aaa2a8f55706e1bdfdc319dff052aa1197a5ef63
Shivaram74 <ramshiva307@gmail.com>
2026-01-14 15:12:33 +0530

    commit

--- login.js
+++ login.js
@@ -1,8 +1,9 @@
 const API="https://private-vault-backend.onrender.com";
-function login(){
+async function login(){
     const username=document.getElementById("username").value;
     const password=document.getElementById("password").value;
-    fetch("${API}/api/login",{
+    try{
+        const res =awaits fetch(`${API}/api/login`,{
         method:"POST",
         headers:{
             "Content-Type":"application/json"
@@ -11,17 +12,17 @@ function login(){
             username:username,
             password:password
         })
-    })
-    .then(response=>response.json())
-    .then(data=>{
-        if(data.token){
-            document.getElementById("result").innerText="Login Successful";
-            localStorage.setItem("token",data.token);
-        }else{
-            document.getElementById("result").innerText="Login Failed";
-        }
-    })
-    .catch(error=>{
-        console.error(error)
-    })
+    });
+    if(!res.ok){
+        const text=await res.text();
+        throw new error(text|| "Login failed");
+    }
+    const data=await res.json();
+
+    localStorage.setItem("token",data.token);
+    window.location.href="dashboard.html";
+}catch(err){
+    console.error(err);
+    document.getElementById("result").innerText="Invalid credentials";
+}
 }
