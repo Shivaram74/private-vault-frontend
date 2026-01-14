c8bc9cdea07dc5523f0355cfc84e72a144763add
Shivaram74 <ramshiva307@gmail.com>
2026-01-14 15:22:02 +0530

    commit

--- login.js
+++ login.js
@@ -1,28 +1,30 @@
-const API="https://private-vault-backend.onrender.com";
-async function login(){
-    const username=document.getElementById("username").value;
-    const password=document.getElementById("password").value;
-    try{
-        const res =awaits fetch(`${API}/api/login`,{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            username:username,
-            password:password
-        })
-    });
-    if(!res.ok){
-        const text=await res.text();
-        throw new error(text|| "Login failed");
-    }
-    const data=await res.json();
+const API_BASE = "https://private-vault-backend.onrender.com";
 
-    localStorage.setItem("token",data.token);
-    window.location.href="dashboard.html";
-}catch(err){
+async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const result = document.getElementById("result");
+
+  try {
+    const res = await fetch(`${API_BASE}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username, password })
+    });
+
+    if (!res.ok) {
+      throw new Error("Login failed");
+    }
+
+    const data = await res.json();
+
+    localStorage.setItem("token", data.token);
+    window.location.href = "dashboard.html";
+
+  } catch (err) {
     console.error(err);
-    document.getElementById("result").innerText="Invalid credentials";
-}
+    result.innerText = "Invalid username or password";
+  }
 }
